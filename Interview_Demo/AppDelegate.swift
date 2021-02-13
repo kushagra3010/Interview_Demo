ee95a0b80cc456b8345a41e0128b13f7d92c48f5
Kushagara <kushagarapandya@Kushagaras-MacBook-Pro.local>
2021-02-13 19:22:22 +0530

    Updated Appdelegate

--- Interview_Demo/AppDelegate.swift
+++ Interview_Demo/AppDelegate.swift
@@ -16,10 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let navigationController = UINavigationController(rootViewController: SearchViewController())
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-        
         return true
     }
 
